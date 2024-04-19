571295d1c31a59635004630356682258c1cdd3a1
LukasdeSouza <lucas@bestdeal.com.br>
2024-04-18 21:29:50 -0300

    feature: criando component que detalhes os dados da row selecionada e adicionando-o ao RootLayout

---
+++ src/components/selectedData/index.jsx
@@ -0,0 +1,19 @@
+import { Stack, Typography } from "@mui/material"
+
+const SelectedData = ({driverName, vehiclePlate}) => {
+  return(
+    <Stack width={'100%'} alignItems={'flex-end'} justifyContent={'end'}>
+      <Typography variant='body1'>
+        Selecionado
+      </Typography>
+      <Typography variant='body1'>
+        Motorista: {driverName}
+      </Typography>
+      <Typography variant='body1'>
+        Veículo: {vehiclePlate}
+      </Typography>
+    </Stack>
+  )
+}
+
+export default SelectedData
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom"
 import MenuAppBar from "../components/appBar"
 import { Stack } from "@mui/material"
+import SelectedData from "../components/selectedData"
 
 const RootLayout = () => {
   return (
     <>
       <MenuAppBar/>
       <Stack px={3}>
+        <SelectedData/>
         <Outlet/>
       </Stack>
     </>
